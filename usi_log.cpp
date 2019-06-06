bbb058099bd5ba3fb65299293bb0082bb1514a16
Dean <dean_yin@usiglobal.com>
2019-06-06 11:24:18 +0800

    debug

--- usi_log.cpp
+++ usi_log.cpp
@@ -1,7 +1,7 @@
 #include <experimental/filesystem>
-#include <phosphor-logging/elog-errors.hpp>
-#include <phosphor-logging/elog.hpp>
-#include <phosphor-logging/log.hpp>
+//#include <phosphor-logging/elog-errors.hpp>
+//#include <phosphor-logging/elog.hpp>
+//#include <phosphor-logging/log.hpp>
 #include <com/usi/Ssdarray/Switch/error.hpp>
 #include "usi_log.hpp"
 
@@ -12,19 +12,19 @@ namespace jboflog
 namespace manager
 {
     
-using namespace phosphor::logging;
+//using namespace phosphor::logging;
 using namespace sdbusplus::com::usi::Ssdarray::Switch::Error;
-namespace Ssdarray = phosphor::logging::com::usi::Ssdarray;
-using SsdLinkFail = Ssdarray::Switch::SsdLinkFailure;
-namespace fs = std::experimental::filesystem;
+//namespace Ssdarray = phosphor::logging::com::usi::Ssdarray;
+//using SsdLinkFail = Ssdarray::Switch::SsdLinkFailure;
+//namespace fs = std::experimental::filesystem;
     
 void Infolog::ssdinfo_create_log(){
     
     //log<level::ERR>("Ssd link fail",
     //                        entry("FILENAME=%s", tarFilePath.c_str()));
     //report<SsdLinkFailure>(SsdLinkFail::PATH(tarFilePath.c_str()));
-    log<level::ERR>("Ssd link fail");
-    //report<SsdLinkFailure>();
+    //log<level::ERR>("Ssd link fail");
+    ///report<SsdLinkFailure>();
 }
     
 }//namespace manager
