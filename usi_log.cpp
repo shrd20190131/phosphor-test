0c0833f997114d4e8dad74ae019754ac68b2da8e
Dean <dean_yin@usiglobal.com>
2019-06-06 17:59:39 +0800

    debug

--- usi_log.cpp
+++ usi_log.cpp
@@ -13,11 +13,12 @@ namespace manager
     
 using namespace phosphor::logging;
 using SsdLinkFail = sdbusplus::com::usi::Ssdarray::Switch::Error::SsdLinkFailure;
+using FAIL = com::usi::Ssdarray::Switch::FAIL;
     
 void Infolog::ssdinfo_create_log(){
     
     log<level::ERR>("Ssd link fail");
-    ///report<SsdLinkFailure>();
+    report<SsdLinkFailure>(FAIL("Ssd link fail"));
 }
     
 }//namespace manager
