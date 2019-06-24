c7b39fe1b9367db1b1dc8fe08788990345f5a579
Dean <dean_yin@usiglobal.com>
2019-06-24 16:50:04 +0800

    debug

--- usi_log.cpp
+++ usi_log.cpp
@@ -117,7 +117,8 @@ std::map<std::string, sdbusplus::message::variant<std::string, std::map<std::str
     error["Error1"] = "The POWER_GOOD signal is negated";
     error["Error2"] = "A temperature fault or warning has occurred";
     psb_error_temp = error;
-    status["PSB"] = sdbusplus::message::variant_ns::get<std::string>(psb_error_temp);
+    //status["PSB"] = sdbusplus::message::variant_ns::get<std::string>(psb_error_temp);
+    status["PSB"] = sdbusplus::message::variant_ns::get<std::map<std::string, std::string>>(psb_error_temp);
 
 /*
     if(!psstatuscall.startwith(psa_getvalue, std::string("0x"))){
