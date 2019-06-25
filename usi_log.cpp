09699af04ddda416edce064f9850f6f8db40b5d1
Dean <dean_yin@usiglobal.com>
2019-06-25 14:49:10 +0800

    psa read fail,psb read fail

--- usi_log.cpp
+++ usi_log.cpp
@@ -118,13 +118,13 @@ std::map<std::string, sdbusplus::message::variant<std::string, std::map<std::str
     //status["PSA"] = sdbusplus::message::variant_ns::get<std::map<std::string, std::string>>(psa_error_temp);
 
     //error.clear();
-    error["Error1"] = "The POWER_GOOD signal is negated";
-    error["Error2"] = "A temperature fault or warning has occurred";
-    error["Error3"] = "A temperature fault or warning has occurred";
-    error["Error4"] = "A temperature fault or warning has occurred";
-    psb_error_temp = error;
-    //status["PSB"] = sdbusplus::message::variant_ns::get<std::string>(none_flag);
-    status["PSB"] = sdbusplus::message::variant_ns::get<std::map<std::string, std::string>>(psb_error_temp);
+    //error["Error1"] = "The POWER_GOOD signal is negated";
+    //error["Error2"] = "A temperature fault or warning has occurred";
+    //error["Error3"] = "A temperature fault or warning has occurred";
+    //error["Error4"] = "A temperature fault or warning has occurred";
+    //psb_error_temp = error;
+    status["PSB"] = sdbusplus::message::variant_ns::get<std::string>(none_flag);
+    //status["PSB"] = sdbusplus::message::variant_ns::get<std::map<std::string, std::string>>(psb_error_temp);
 
 /*
     if(!psstatuscall.startwith(psa_getvalue, std::string("0x"))){
